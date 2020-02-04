77b78ea65574f2b6fac921fb17ac15b1cda95f1f
Goku-San <gokusan838@gmail.com>
2020-02-04 18:23:43 +0100

    Configure app

--- config/application.rb
+++ config/application.rb
@@ -22,10 +22,8 @@ module TwitterLikeApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configure time zone
+    config.time_zone = 'Europe/Skopje'
 
     # Don't generate system test files.
     config.generators.system_tests = nil
