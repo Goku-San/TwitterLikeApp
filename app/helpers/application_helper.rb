57e1d02492b844656881eb110c595d3751bcadb6
Goku-San <gokusan838@gmail.com>
2020-02-10 14:39:16 +0100

    Added StaticPagesController#contact - Created appropriate view - Added appropriate route Added dynamic title in layouts/application.slim as well as in static_pages views Refactored the static_pages_controller test and added test for contact

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title page_title = ''
+    base_title = "TwitterLikeApp"
+
+    return base_title if page_title.empty?
+
+    page_title + " | " + base_title
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,20 +1,45 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "TwitterLikeApp"
+  end
+
   test "should get home" do
     get root_url
+
     assert_response :success
+
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
-    get help_url
     assert_recognizes({ controller: "static_pages", action: "help" }, "help")
+
+    get help_url
+
     assert_response :success
+
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get about_url
     assert_recognizes({ controller: "static_pages", action: "about" }, "about")
+
+    get about_url
+
     assert_response :success
+
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    assert_recognizes({ controller: "static_pages", action: "contact" }, "contact")
+
+    get contact_url
+
+    assert_response :success
+
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
